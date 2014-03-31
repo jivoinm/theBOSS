77b3d5b5304bc48bc3608e582a5f1ac5e084b932
mircea <jivoinm@gmail.com>
2014-03-31 18:38:47 -0400

    added new tests

---
+++ test/client/spec/filters/timeago.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Filter: timeago', function () {
+
+  // load the filter's module
+  beforeEach(module('theBossApp'));
+
+  // initialize a new instance of the filter before each test
+  var timeago;
+  beforeEach(inject(function ($filter) {
+    timeago = $filter('timeago');
+  }));
+
+  it('should return less than a minute ago when in date now:', function () {
+    var text = new Date();
+    expect(timeago(text)).toBe('less than a minute ago');
+  });
+
+});
