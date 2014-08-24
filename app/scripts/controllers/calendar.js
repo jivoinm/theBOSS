f64b504c8c54650610299366f8aecde7f29878aa
Mirca Zivojin <jivoinm@gmail.com>
2014-08-24 10:14:52 -0400

    new updates

--- app/scripts/controllers/calendar.js
+++ app/scripts/controllers/calendar.js
@@ -2,8 +2,12 @@
 
 angular.module('theBossApp')
 
-  .controller('CalendarCtrl',['$scope', '$routeParams', function ($scope, $routeParams) {
+  .controller('CalendarCtrl',['$scope', '$routeParams', '$location', function ($scope, $routeParams, $location) {
         $scope.$parent.pageHeader = 'Calendar';
         $scope.orderStatus = $routeParams.status;
+        $scope.queryText = '';
+        $scope.loadOrders = function(){
+        	$location.search('text',$scope.queryText);
+        }
 		
   }]);
