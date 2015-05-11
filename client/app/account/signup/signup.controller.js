e32013b4a0f0f6f3d99c83548d737a41110e8e9c
Mirca Zivojin <jivoinm@gmail.com>
2015-05-11 15:54:33 +0300

    new changes added

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -5,31 +5,35 @@ angular.module('theBossApp')
     $scope.user = {};
     $scope.errors = {};
 
-    $scope.register = function(form) {
+    $scope.register = function (form) {
       $scope.submitted = true;
 
-      if(form.$valid) {
-        Auth.createUser({
-          name: $scope.user.name,
-          email: $scope.user.email,
-          password: $scope.user.password
-        })
-        .then( function() {
-          // Account created, redirect to home
-          $location.path('/');
-        })
-        .catch( function(err) {
-          err = err.data;
-          $scope.errors = {};
+      if (form.$valid) {
+          var user = {
+              owner: 'DelPriore',
+              name: $scope.user.name,
+              email: $scope.user.email,
+              role: $scope.user.role,
+              password: $scope.user.password
+          };
 
-          // Update validity of form fields that match the mongoose errors
-          angular.forEach(err.errors, function(error, field) {
-            form[field].$setValidity('mongoose', false);
-            $scope.errors[field] = error.message;
-          });
-        });
-      }
-    };
+          Auth.createUser(user)
+              .then(function () {
+                  // Account created, redirect to home
+                  $location.path('/user');
+              })
+              .catch(function (err) {
+                  err = err.data;
+                  $scope.errors = {};
+
+                  // Update validity of form fields that match the mongoose errors
+                  angular.forEach(err.errors, function (error, field) {
+                      form[field].$setValidity('mongoose', false);
+                      $scope.errors[field] = error.message;
+                  });
+              });
+            }
+        };
 
     $scope.loginOauth = function(provider) {
       $window.location.href = '/auth/' + provider;
--- client/components/auth/auth.service.js
+++ client/components/auth/auth.service.js
@@ -61,8 +61,8 @@ angular.module('theBossApp')
 
         return User.save(user,
           function(data) {
-            $cookieStore.put('token', data.token);
-            currentUser = User.get();
+            //$cookieStore.put('token', data.token);
+            //currentUser = User.get();
             return cb(user);
           },
           function(err) {
